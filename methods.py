b3536f6084c2c008a83dc8992ac9ea17c7074033
Evan Canoy <evan@Evans-MacBook-Pro.local>
2019-03-04 10:01:11 -0500

    final :(

--- methods.py
+++ methods.py
@@ -6,6 +6,23 @@
 #
 # So if the number 5 is passed in, num_list_with_arg should return [1, 2, 3, 4]
 
+def num_list_with_arg(i):
+    if i > 1:
+        count = 1
+        emptyList = []
+        if count < i:
+            emptyList.append(count)
+            count += 1
+        else:
+            return emptyList
+    else:
+        return emptyList
+
+num_list_with_arg(5)
+
+
+        
+
 
 # #2: Modify the has_ruby_exp method below so that it returns a SORTED list of
 # all instructors who have Ruby experience (i.e. where "ruby" == True)
@@ -49,6 +66,10 @@ def has_ruby_exp():
             'pasta': False
         }
     }
+    # for instructors in experience:
+    #     if instructors.ruby == True:
+    #         ruby_experience.append(instructors)
+    #     return ruby_experience
     
 
 
@@ -57,6 +78,19 @@ def has_ruby_exp():
 # argument is an integer, convert it to a string and return the string; If the
 # argument is neither a string nor an integer, return the string "this is not a
 # str or a int":
+
+def toggle_str_num(arg):
+    if type(arg) == str:
+        arg_int = int(arg)
+        return print(arg_int)
+    elif type(arg) == int:
+        arg_str = str(arg)
+        return print(arg_str)
+    else:
+        return print('this is not a str or int')
+
+toggle_str_num('8')
+toggle_str_num(8)
 
 
 # Commit when you finish working on these questions!
